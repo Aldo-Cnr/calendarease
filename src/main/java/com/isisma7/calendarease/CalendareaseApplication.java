2a643600c9b85192f6262ebf561f7c89405fbf26
Aldo-Cnr <aldoconrado2512@gmail.com>
2024-06-01 08:49:16 -0600

    Inicializacion del Proyecto

---
+++ src/main/java/com/isisma7/calendarease/CalendareaseApplication.java
@@ -0,0 +1,13 @@
+package com.isisma7.calendarease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendareaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendareaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isisma7/calendarease/CalendareaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isisma7.calendarease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendareaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
